63e12991994729ab08732977bb8cdcb1d0027c76
李瑞 <1102563860@qq.com>
2021-02-24 15:03:35 +0800

    登录注册逻辑完成

--- target/classes/com/springboot/Repository/Impl/UserServiceImpl.java
+++ target/classes/com/springboot/Repository/Impl/UserServiceImpl.java
@@ -1,32 +1,138 @@
 package com.springboot.Repository.Impl;
 
 import com.springboot.Repository.UserService;
-import com.springboot.entity.User;
+import com.springboot.entity.*;
+import com.springboot.mapper.AddressMapper;
+import com.springboot.mapper.GwcMapper;
+import com.springboot.mapper.OrderMapper;
 import com.springboot.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.lang.reflect.Parameter;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
 
 
 @Service
 public class UserServiceImpl implements UserService {
     @Autowired
-    UserMapper user;
+    UserMapper userMapper;
+
+    //购物车接口
+    @Autowired
+    GwcMapper gwc;
+
+    //收货地址接口
+    @Autowired
+    AddressMapper address;
+
+    //订单接口
+    @Autowired
+    OrderMapper orderMapper;
+
 
     /**
-     * 根据用户账号查询用户信息
-     * 得到用户信息 比对 输入的密码是否与用户信息密码一致
-     * @param id
-     * @param password
+     * 用户注册逻辑
+     * @param user
      * @return
      */
     @Override
-    public User dealLogin(String id,String password) {
-        User user = this.user.getUser(id);
-        if (null != user && user.getUser_password().equals(password)) {
-            return user;
+    public String registerUser(User user) {
+        //判断账号是否存在
+        if (userMapper.getUser(user.getUser_id())==null){
+            return userMapper.insertUser(user) > 0 ? "注册成功" : "注册失败";
         }
-        return null;
+        return "该账号已被使用";
     }
+
+
+    /**
+     * 用户登录逻辑
+     * @param user
+     * @return
+     */
+    @Override
+    public String dealLogin(User user) {
+        //通过用户账号获取用户
+        User user_login = userMapper.getUser(user.getUser_id());
+        //获取失败
+        if (user_login==null) {
+            return "该账号不存在";
+        }
+        //密码错误
+        if (!user_login.getUser_password().equals(user.getUser_password())){
+            return "密码错误";
+        }
+        //若账号存在密码相同，登录成功
+        return "登录成功";
+    }
+
+    //更改购物车食物数量
+    @Override
+    public void updateNum(int custId, int foodId, int num) {
+         gwc.updateNum(num,custId,foodId);
+    }
+
+    //删除购物车食物
+    @Override
+    public void removeGwc(int custid, int foodid) {
+        System.out.println(custid+"," + foodid);
+        gwc.removeGwc(custid,foodid);
+    }
+
+    @Override
+    public void addGwc(Gwc g) {
+        //SELECT food_count FROM t_gwc WHERE food_id = 8
+        ArrayList<Integer> list = gwc.findById(g.getFood_id(),g.getCust_id());
+        if(list.size() >= 1){
+            gwc.updateNum(list.get(0) + g.getFood_count(),g.getCust_id(),g.getFood_id());
+        } else {
+            gwc.add(g);
+        }
+    }
+
+    @Override
+    public OrderModel dealConfirmOrder(int userId, String gwcId) {
+        String[] str = gwcId.split("AND");
+        OrderModel model = new OrderModel();
+        double num = 0;
+        for (String st : str){
+            Gwc g = gwc.findByUserId(Integer.valueOf(st),userId);
+            num += g.getFood_count()*g.getFood_price();
+            model.getOrder_food().add(g);
+        }
+        int order_bus = gwc.getBusIdByGwcId(Integer.valueOf(str[0]));
+        model.setOrder_price(num);
+        model.setOrder_user(userId);
+        model.setOrder_bus(order_bus);
+
+        return model;
+    }
+
+    @Override
+    public List<Address> getAddress(int userId) {
+        return address.getAddress(userId);
+    }
+
+    @Override
+    public String submitOrder(Order order) {
+        Date date = new Date();
+        SimpleDateFormat ft = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        order.setOrder_date(ft.format(date));
+        Integer orderId = UUID.randomUUID().toString().hashCode();
+        orderId = orderId < 0 ? -orderId : orderId;
+        String uuid = String.valueOf(orderId);
+        order.setOrder_id(uuid);
+        orderMapper.addOrder(order);
+        gwc.updateState(order.getOrder_user(),order.getOrder_bus());
+
+        return uuid;
+    }
+
+
+
 }
--- target/classes/com/springboot/Repository/UserService.java
+++ target/classes/com/springboot/Repository/UserService.java
@@ -1,9 +1,31 @@
 package com.springboot.Repository;
 
 
-import com.springboot.entity.User;
+import com.springboot.entity.*;
+
+import java.util.List;
 
 public interface UserService {
-    //获取用户id和密码
-    public User dealLogin(String id, String password);
+    //用户登录
+    String dealLogin(User user);
+
+    //用户注册
+    String registerUser(User user);
+
+    //更改购物车食物数量
+    void updateNum(int custId, int foodId, int num);
+
+    //删除购物车食物
+    void removeGwc(int custid, int foodid);
+
+    void addGwc(Gwc gwc);
+
+    OrderModel dealConfirmOrder(int userId, String gwcId);
+
+    List<Address> getAddress(int userId);
+
+    String submitOrder(Order order);
+
+
+
 }
